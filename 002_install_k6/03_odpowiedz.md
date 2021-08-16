0017730a75c3e097c0256766eef3c5843d597110
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-16 07:52:23 +0200

    removed solutiuon files

--- 002_install_k6/03_odpowiedz.md
+++
@@ -1,6 +0,0 @@
-```powershell
-    k6 convert -O .\03_odpowiedz.js .\03_odpowiedz.har
-    k6 run --http-debug=full .\03_odpowiedz.js
-``` 
-
-
--- 013_modules/04_envs.js
+++
@@ -1,8 +0,0 @@
-export const envs = {
-    'QA' : {
-        BASE_URL: 'https://appxx.azurewebsites.net/'
-    },
-    'STAGE': {
-        BASE_URL: 'https://APPXX.azurewebsites.net/'
-    }
-}
